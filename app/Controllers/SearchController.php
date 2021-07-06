171b2fa411226ed68ecff843658b2276ce152e72
Melaniaift <ioanamelany99@yahoo.com>
2021-07-06 17:45:58 +0300

    search

---
+++ app/Controllers/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class SearchController extends Controller 
+{
+    public function index(){
+        return view('search_form');
+    }
+    public function search(){
+        $search=$this->request->getVar('search');
+        $db=\Config\Database::connect();
+        $builder=$db->table('flori');
+        $builder->like('nume',$search);$builder->orLike('culoare',$search);
+        $query=$builder->get();
+        $q=$query->getResultArray();
+        
+        $data['r']=$q;
+        return view('search_result',$data);
+    }
+}
+
